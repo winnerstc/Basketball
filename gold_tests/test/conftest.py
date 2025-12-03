d6a41c3dd207aaf1f6ba2c2c5555a0e9a635ac0b
Nick <Nicholas.Steen@techconsulting.tech>
2025-12-03 13:21:51 -0500

    :)

--- gold_tests/test/conftest.py
+++ gold_tests/test/conftest.py
@@ -13,18 +13,18 @@ def spark():
     return spark
 
 
-@pytest.fixture(scope="session")
-def gold_tables(spark):
-    gold_db = "nba_gold"
+# @pytest.fixture(scope="session")
+# def gold_tables(spark):
+#     gold_db = "nba_gold"
 
-    tables = {
-        "assists_pg": spark.table(f"{gold_db}.player_assists_per_game_gold"),
-        "rebounds_pg": spark.table(f"{gold_db}.player_rebounds_per_game_gold"),
-        "win_pct": spark.table(f"{gold_db}.team_win_percentage_gold"),
-        "points_pg": spark.table(f"{gold_db}.player_points_per_game_gold"),
-        "pts_season": spark.table(f"{gold_db}.player_total_points_per_season_gold"),
-        "ast_season": spark.table(f"{gold_db}.player_total_assists_per_season_gold"),
-        "reb_season": spark.table(f"{gold_db}.player_total_rebounds_per_season_gold"),
-        "dd_wins": spark.table(f"{gold_db}.team_double_digit_wins_per_season_gold"),
-    }
-    return tables
+#     tables = {
+#         "assists_pg": spark.table(f"{gold_db}.player_assists_per_game_gold"),
+#         "rebounds_pg": spark.table(f"{gold_db}.player_rebounds_per_game_gold"),
+#         "win_pct": spark.table(f"{gold_db}.team_win_percentage_gold"),
+#         "points_pg": spark.table(f"{gold_db}.player_points_per_game_gold"),
+#         "pts_season": spark.table(f"{gold_db}.player_total_points_per_season_gold"),
+#         "ast_season": spark.table(f"{gold_db}.player_total_assists_per_season_gold"),
+#         "reb_season": spark.table(f"{gold_db}.player_total_rebounds_per_season_gold"),
+#         "dd_wins": spark.table(f"{gold_db}.team_double_digit_wins_per_season_gold"),
+#     }
+#     return tables
